24f17b1eed8c80c5d46e52625fe9c6e3c2d35956
Julie Cappello <jcappello@bigpanda.io>
2020-07-09 19:37:14 -0500

    code submission

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const request = require('request');
+const got = require('got');
 const crypto = require('crypto');
 
-const user = process.env.username;
-const api_token = process.env.token;
+const github_base = 'https://api.github.com';
 
-//permission set for master branch
-const permissions = {
+//protection set for master branch
+const protections = {
     "required_status_checks": null,
     "enforce_admins": true,
     "required_pull_request_reviews": null,
@@ -22,147 +21,99 @@ const permissions = {
     "allow_deletions": true
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
 
     console.log("Event: " + JSON.stringify(event, null, 2));
     console.log("Body: " + event.body);
 
-    let response = {};
+    try {
+        let secret = event.headers['x-hub-signature'];
+        console.log ('Header: ' + secret);
+        if (secret && verify_signature(secret, event.body)) {
+            let body = JSON.parse(event.body);
 
-    let secret = event.headers['X-Hub-Signature'];
-    let payload_body = event.body;
-
-    //verify signatures match before proceeding
-    if (secret && verify_signature(secret, payload_body)) {
-
-        let body = JSON.parse(event.body);
-
-        if (body.hasOwnProperty('action')) {
-            //check if a repo was created
-            if (body['action'] == 'created' && body.hasOwnProperty('repository')) {
-                let repo = body.repository['full_name'];
-
-                let protectFailed = false;
-                protectMaster(repo, (protectError, protectResult) => {
-                    if(protectError){
-                        protectFailed = true;
+            if (body.hasOwnProperty('action') && body.hasOwnProperty('repository')) {
+                //check if a repo was created
+                if (body['action'] == 'created') {
+                    let repo = body.repository['full_name'];
+                    console.log(repo);
+                    //protect master branch
+                    const protectResponse = await protectMaster(repo);
+                    if (protectResponse.statusCode !== 200) {
+                        return {statusCode: protectResponse.statusCode, body: protectResponse.body};
                     }
-                    //post issue with result
-                    notifyIssue(repo, protectResult, (notifyError, issueResult) => {
-                        console.log(protectFailed);
-                        if (protectFailed && notifyError) {
-                            response.statusCode = 500;
-                            response.body = JSON.stringify('Operation failed');
-                            callback(null, response);
-                        }
-                        if (notifyError) {
-                            response.statusCode = issueResult.statusCode;
-                            response.body = issueResult.body;
-                            callback(null, response);
-                        } else {
-                            response.statusCode = 200;
-                            response.body = JSON.stringify('Master branch protection enabled');
-                            callback(null, response);
-                        }
-                    });
-                });
+                    //notify of protection in issue
+                    const issueResponse = await notifyIssue(repo, protectResponse);
+                    if (issueResponse.statusCode !== 201) {
+                        return {statusCode: issueResponse.statusCode, body: issueResponse.body};
+                    }
+                }
+                console.log('Operation completed successfully.');
+                return {statusCode: 200, body: JSON.stringify('Success')};
+            } else {
+                return {statusCode: 400, body: JSON.stringify('Request body has no repository action. Make sure your webhook is enabled to send repository events.')};
             }
         }
-
-    } else {
-        response.statusCode = 403;
-        response.body = JSON.stringify('Signatures do not match. Halting operation.');
-        callback(null, response);
+        else { return {statusCode: 403, body: JSON.stringify('Signature does not match. Aborting operation.')}; }
+    }
+    catch (error){
+        console.log(error);
+        return {statusCode: 500, body: JSON.stringify('Operation could not be completed.')};
     }
 };
 
-const protectMaster = (repo, callback) => {
-    request({
-        body: JSON.stringify(permissions),
-        method: "PUT",
-        url: `https://api.github.com/repos/${repo}/branches/master/protection`,
+const initializeRequest = () => {
+    let encodedCredentials = new Buffer.from(
+        process.env.username + ":" + process.env.token
+    );
+    let authHeader = "Basic " + encodedCredentials.toString("base64");
+    return {
+        json: {},
         headers: {
-            "User-Agent": "script",
-            "Content-Type": "application/json"
+            'Authorization': authHeader,
+            'User-Agent': 'script',
+            'Content-Type': 'application/json'
         },
-        auth: {
-            "username": user,
-            "password": api_token
-        },
-    }, (error, response) => {
-        let res = {};
-        if (error || response === undefined) {
-            res.statusCode = 500;
-            res.body = JSON.stringify({"message": error});
-            callback(new Error("Could not complete operation."), response);
-        }
-        if(response.statusCode === 200) {
-            callback(null, response);
-        }
-        else{
-            res.statusCode = response.statusCode;
-            callback(new Error("Could not complete operation."), response);
-        }
-    });
+        throwHttpErrors: false
+    };
 };
 
-const notifyIssue = (repo, result, callback) => {
-    let issue_text = {};
-    if(result.statusCode === 200){
-        issue_text = {
-            "title": "Master branch protection enabled.",
-            "body": "@jcappello \n The following permissions have been set: \n `" + JSON.stringify(permissions) + "`"
-        };
+const protectMaster =  async (repo) => {
+    try {
+        let request = initializeRequest();
+        request.json = protections;
+        const response = await got.put(`${github_base}/repos/${repo}/branches/master/protection`, request);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 500, body: JSON.stringify('Operation could not be completed.') };
     }
-    else{
-        issue_text = {
-            "title": "Master branch protections could not be enabled at this time.",
-            "body": "@jcappello \n The following error occured: \n `" + result.body + "`"
-        };
-    }
-    console.log(issue_text);
-    request({
-        body: JSON.stringify(issue_text),
-        method: "POST",
-        url: `https://api.github.com/repos/${repo}/issues`,
-        headers: {
-            "User-Agent": "script",
-            "Content-Type": "application/json"
-        },
-        auth: {
-            "username": user,
-            "password": api_token
-        },
-    }, (error, response) => {
-        let res = {};
-        if (error || response === undefined) {
-            res.statusCode = 500;
-            res.body = JSON.stringify({"message": error});
-            callback(new Error("Could not complete operation."), response);
-        }
-        if(response.statusCode === 201) {
-            callback(null, response);
-        }
-        else{
-            res.statusCode = response.statusCode;
-            callback(new Error("Could not complete operation."), response);
-        }
-    });
 };
 
+const notifyIssue =  async (repo, protections) => {
+    try {
+        let request = initializeRequest();
+        let protect = protections;
+        request.json = {
+            'title': 'Master branch protection enabled.',
+            'body': '@jcappello \n The following permissions have been set: \n `' + protect + '`'
+        };
+        const response = await got.post(`${github_base}/repos/${repo}/issues`, request);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 500, body: JSON.stringify('Operation could not be completed.') };
+    }
+};
 
-function verify_signature(secret, payload_body, callback){
+function verify_signature(secret, payload_body){
     let hash = crypto.createHmac('sha1', process.env.secret)
         .update(payload_body)
+        .update('test')
         .digest('hex');
     let signature = 'sha1=' + hash;
     console.log('Compare: ' + signature);
     if (!crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(secret))){
-        let response = {
-            statusCode: 500,
-            body: JSON.stringify('Signature does not match. Aborting operation.'),
-        };
-        callback(null, response);
-    }
-    else return true;
+        return false;
+    } else return true;
 };
